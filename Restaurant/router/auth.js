13caf869f06ad7be9fb3680e418c25f947e40b11
Sebastian_Gil <s.gil@utp.edu.co>
2022-08-19 10:30:36 -0500

    Authorization middleware

--- Restaurant/router/auth.js
+++ Restaurant/router/auth.js
@@ -3,8 +3,6 @@ const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const config = require('config');
 
 router.post('/', async (req, res) => {
     const {error} = validate(req.body);
@@ -16,7 +14,7 @@ router.post('/', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
     
-    const token = jwt.sign({_id: user._id, name:user.name}, config.get('jwtPrivateKey'));
+    const token = user.generateAuthToken(); 
     res.send(token);
 });
 
